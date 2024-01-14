8a147b03cf1f9d4002295bc77d1441128d291dc2
osamosonok <osamosonok@playtika.com>
2024-01-14 21:42:15 +0200

    Add react as lib 2.0

--- script.js
+++ script.js
@@ -1,19 +1,208 @@
+var _this = this;
+
 var domContainer = document.querySelector('#root');
 var root = ReactDOM.createRoot(domContainer);
+
+var x = 10;
+var list = ['value1', 'value2', 'value3'];
+var cars = ['bmw', 'audi', 'mercedes'];
+
+var objects = [{
+    id: 1,
+    name: 'admin',
+    role: 'IT'
+}, {
+    id: 2,
+    name: 'CEO',
+    role: 'IT'
+}];
+
+// complex object
+obj = {
+    id: 1,
+    name: 'Alex',
+    age: 45,
+    child: {
+        type: 'son'
+    }
+
+    // destructuring
+    // {objName, age, boy:child.type} = obj;
+
+};var showText = true;
+
+// 1. Creating new component (With Capital letter) as a function
+var List = function List(props) {
+    console.log(props);
+
+    return props.arr.length ? React.createElement(
+        'ul',
+        null,
+        props.arr.map(function (item, index) {
+            return React.createElement(
+                'li',
+                { key: index },
+                item
+            );
+        })
+    ) : null;
+};
+
+// if arr argument is absent use empty array
+var ListAdvanced = function ListAdvanced(_ref) {
+    var _ref$arr = _ref.arr,
+        arr = _ref$arr === undefined ? [] : _ref$arr,
+        _ref$tag = _ref.tag,
+        tag = _ref$tag === undefined ? "ol" : _ref$tag;
+
+    var CustomTag = '' + tag; //<ul>, <ol>
+    return arr.length ? React.createElement(
+        CustomTag,
+        null,
+        arr.map(function (item, index) {
+            return React.createElement(ListItem, { key: index, item: item });
+        })
+    ) : null;
+};
+
+// List that could be dynamically updated by react, need to use state for rendering after update
+var DynamicallyUpdatedList = function DynamicallyUpdatedList(_ref2) {
+    var _ref2$arr = _ref2.arr,
+        arr = _ref2$arr === undefined ? [] : _ref2$arr,
+        _ref2$tag = _ref2.tag,
+        tag = _ref2$tag === undefined ? "ol" : _ref2$tag;
+
+    var CustomTag = '' + tag; //<ul>, <ol>
+
+    _this.state = {
+        arr: arr
+    };
+
+    stateArr = _this.state.arr;
+
+    return arr.length ? React.createElement(
+        CustomTag,
+        null,
+        stateArr.map(function (item, index) {
+            return React.createElement(ListItem, { key: index, item: item });
+        })
+    ) : null;
+};
+
+//list with destructuring
+var ListItem = function ListItem(_ref3) {
+    var index = _ref3.index,
+        item = _ref3.item;
+
+    return React.createElement(
+        'li',
+        { key: index },
+        item
+    );
+};
+
+var Button = function Button(_ref4) {
+    var text = _ref4.text;
+
+    return React.createElement(
+        'button',
+        null,
+        text
+    );
+};
+
+//Add new element to list after delay
+setTimeout(function () {
+    console.log('After 1000ms');
+    list.push('value4');
+    console.log(list);
+}, 1000);
+
 var helloWorldConst = React.createElement(
-    'div',
+    React.Fragment,
     null,
     React.createElement(
         'h1',
-        null,
+        { className: 'heading' },
         'Hello world'
     ),
     React.createElement(
         'h2',
         null,
-        'h2 text'
-    )
+        'h2 const ',
+        x
+    ),
+    React.createElement(List, { arr: cars }),
+    React.createElement(ListAdvanced, { arr: list, tag: "ol" }),
+    React.createElement(DynamicallyUpdatedList, { arr: list, tag: "ul" }),
+    React.createElement(
+        'table',
+        null,
+        React.createElement(
+            'tbody',
+            null,
+            React.createElement(
+                'tr',
+                null,
+                list.map(function (item, index) {
+                    return React.createElement(
+                        'td',
+                        { key: index },
+                        item
+                    );
+                })
+            )
+        )
+    ),
+    React.createElement(
+        'table',
+        { className: 'users' },
+        React.createElement(
+            'thead',
+            null,
+            React.createElement(
+                'tr',
+                null,
+                React.createElement(
+                    'th',
+                    null,
+                    'Name'
+                ),
+                React.createElement(
+                    'th',
+                    null,
+                    'Role'
+                )
+            )
+        ),
+        React.createElement(
+            'tbody',
+            null,
+            objects.map(function (item, index) {
+                return React.createElement(
+                    'tr',
+                    { key: item.id },
+                    Object.keys(item).filter(function (el) {
+                        return el !== 'id';
+                    }).map(function (el, i) {
+                        return React.createElement(
+                            'td',
+                            { key: i },
+                            item[el]
+                        );
+                    })
+                );
+            })
+        )
+    ),
+    showText ? React.createElement(
+        'h3',
+        null,
+        'Conditional text '
+    ) : null,
+    React.createElement(Button, { text: 'Delete' })
 );
+
 root.render(helloWorldConst);
 
 //development here, after
--- src/script.js
+++ src/script.js
@@ -1,8 +1,132 @@
 const domContainer = document.querySelector('#root');
 const root = ReactDOM.createRoot(domContainer);
-const helloWorldConst = <div>
-    <h1>Hello world</h1>
-    <h2>h2 text</h2></div>;
+
+const x = 10
+const list = ['value1', 'value2', 'value3'];
+const cars = ['bmw', 'audi', 'mercedes'];
+
+const objects = [
+    {
+        id: 1,
+        name: 'admin',
+        role: 'IT'
+    },
+    {
+        id: 2,
+        name: 'CEO',
+        role: 'IT'
+    },
+]
+
+// complex object
+obj = {
+    id: 1,
+    name: 'Alex',
+    age: 45,
+    child: {
+        type: 'son'
+    }
+}
+
+// destructuring
+// {objName, age, boy:child.type} = obj;
+
+const showText = true;
+
+// 1. Creating new component (With Capital letter) as a function
+const List = (props) => {
+    console.log(props)
+
+    return props.arr.length
+        ? <ul>
+            {props.arr.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+        : null
+}
+
+// if arr argument is absent use empty array
+const ListAdvanced = ({arr=[], tag="ol"}) => {
+    const CustomTag = `${tag}`; //<ul>, <ol>
+    return arr.length
+        ? <CustomTag>
+            {arr.map((item, index) => <ListItem key ={index} item={item}/>)}
+        </CustomTag>
+        : null
+}
+
+// List that could be dynamically updated by react, need to use state for rendering after update
+const DynamicallyUpdatedList = ({arr=[], tag="ol"}) => {
+    const CustomTag = `${tag}`; //<ul>, <ol>
+
+    this.state = {
+        arr
+    }
+
+    stateArr = this.state.arr
+
+    return arr.length
+        ? <CustomTag>
+            {stateArr.map((item, index) => <ListItem key ={index} item={item}/>)}
+        </CustomTag>
+        : null
+}
+
+//list with destructuring
+const ListItem = ({index, item}) => {
+    return <li key={index}>{item}</li>;
+}
+
+const Button = ({text}) => {
+    return <button>{text}</button>
+}
+
+//Add new element to list after delay
+setTimeout(() => {
+    console.log(`After 1000ms`);
+    list.push('value4');
+    console.log(list)
+}, 1000)
+
+const helloWorldConst = <React.Fragment>
+    <h1 className="heading">Hello world</h1>
+    <h2>h2 const {x}</h2>
+
+    <List arr = {cars}/>
+    <ListAdvanced arr ={list} tag={"ol"}/>
+    <DynamicallyUpdatedList arr ={list} tag={"ul"}/>
+
+
+    {/* 2. Generating table with map */}
+    <table>
+        <tbody>
+        <tr>
+            {list.map((item, index) => <td key = {index}>{item}</td>)}
+        </tr>
+        </tbody>
+    </table>
+
+    {/* 3. Generating table of objects with map */}
+    <table className="users">
+        <thead>
+        <tr><th>Name</th><th>Role</th></tr>
+        </thead>
+        <tbody>
+        {objects.map((item, index) =>
+            <tr key = {item.id}>
+                {Object.keys(item)
+                    .filter(el => el !== 'id')
+                    .map((el, i) => <td key = {i}>{item[el]}</td>)}
+            </tr>)}
+        </tbody>
+    </table>
+
+    {/* 4. Conditional rendering */}
+    {showText ? <h3>Conditional text </h3> : null}
+
+    <Button text="Delete"/>
+
+</React.Fragment>;
+
 root.render(helloWorldConst);
 
 //development here, after
